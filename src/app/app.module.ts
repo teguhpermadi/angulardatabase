a5aeb3fded02616876681992f675e7eb7270cc64
teguhpermadi <tguh.permadi@gmail.com>
2019-09-25 11:26:17 +0700

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ 
+                  BrowserModule,
+                  AngularFireModule.initializeApp(environment.firebaseConfig),
+                  AngularFirestoreModule,
+                  FormsModule,
+                  BrowserAnimationsModule,
+                  ToastrModule.forRoot()
+                ],
+  declarations: [ 
+                  AppComponent, 
+                  HelloComponent,       
+                  EmployeesComponent,
+                  EmployeeComponent,
+                  EmployeeListComponent 
+                ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCl-_qE8VdEp_yNX0IIMUROD4YU92PvOjY",
+    authDomain: "teguh-project.firebaseapp.com",
+    databaseURL: "https://teguh-project.firebaseio.com",
+    projectId: "teguh-project",
+    storageBucket: "teguh-project.appspot.com",
+    messagingSenderId: "399874588334",
+    appId: "1:399874588334:web:094d86e957bcee0e"
+  }
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
